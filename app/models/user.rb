fbe46fa4d6c02dbc241d53597cc68f81259973f1
Temmarie <temmar_whyte16@yahoo.com>
2020-07-10 14:09:14 +0100

    created events attendees

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   has_many :events, :foreign_key => :creator_id
   validates :email, presence: true, uniqueness: true
 
+  has_many :attended_events,  :through => :event_attendees
+  has_many :event_attendees,  :foreign_key => :attendee_id
 end
