c6629da35bc90026af5ea044ab170408753302ca
Temmarie <temmar_whyte16@yahoo.com>
2020-07-13 11:45:12 +0100

    made more error fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,11 @@
 
 class User < ApplicationRecord
   has_secure_password
+  has_many :events, foreign_key: :creator_id
+
+  has_many :event_attendees,  foreign_key: :attendee_id
   has_many :attended_events, through: :event_attendees
 
-  has_many :events, foreign_key: :creator_id
-  has_many :event_attendees,  foreign_key: :attendee_id
   validates :email, presence: true, uniqueness: true
 
   def upcoming_events
