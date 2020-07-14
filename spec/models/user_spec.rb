e7fc382548501f28c9b29283e837fb64d729a811
Temmarie <temmar_whyte16@yahoo.com>
2020-07-14 14:14:35 +0100

    finished user rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,10 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email) }
   end
+
+  describe 'associations' do
+    it { should have_many(:events).with_foreign_key('creator_id') }
+    it { should have_many(:event_attendees).with_foreign_key('attendee_id') }
+    it { should have_many(:attended_events).through(:event_attendees) }
+  end
 end
