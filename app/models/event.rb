e92eca4f1682dbea20803654531b9ba0fec98a60
kotoku-code <kennedy.kotoku@gmail.com>
2020-07-14 15:38:54 +0200

    rspec for event

--- app/models/event.rb
+++ app/models/event.rb
@@ -5,6 +5,9 @@ class Event < ApplicationRecord
   has_many :event_attendees, foreign_key: :attended_event_id
   has_many :attendees, through: :event_attendees
 
+  validates :title, presence: true, length: { minimum: 5 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 100 }
+  
   scope :upcoming, -> { where('Date >= ?', Date.today).order('Date ASC') }
   scope :past, -> { where('Date <  ?', Date.today).order('Date DESC') }
 end
