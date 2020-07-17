3cd30188e03e4a1cfbb584ff9fb0e31b379bc461
Temmarie <temmar_whyte16@yahoo.com>
2020-07-17 09:10:38 +0100

    added users evnts

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -23,7 +23,7 @@ class EventsController < ApplicationController
   # POST /events
   # POST /events.json
   def create
-    @event = Event.new(event_params)
+    @event = current_user.events.build(event_params)
 
     respond_to do |format|
       if @event.save
